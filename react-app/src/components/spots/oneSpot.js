f52d3164d898647e0fa237ccffc16cab0d4f3d5f
christopher <cpannella3@gmail.com>
2022-11-25 12:25:54 -0500

    committal :

--- react-app/src/components/spots/oneSpot.js
+++ react-app/src/components/spots/oneSpot.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink, useHistory, useParams } from "react-router-dom";
 import { fetchOneSpot, fetchSpots } from "../../store/spots";
+import EditSpotForm from "../spotForms/editSpotForm";
 
 
 export default function OneSpot(){
@@ -18,8 +19,23 @@ export default function OneSpot(){
   const filteredSpot = spots?.filter(spot => spot.id == +id)[0]
   console.log("THIS IS THE FILTERED SPOT",filteredSpot)
   const reviews = filteredSpot?.reviews
+
+  const reviewsLength = reviews?.length
   // const images = filteredSpot.
   const spotImages = filteredSpot?.spotImages
+
+
+  const avgRating = (arr) => {
+    let sum = 0
+    for(let i = 0; i < arr?.length; i++){
+        let review = arr[i]
+        sum += review.ratings
+    }
+    return sum/arr?.length
+  }
+
+  const spotAvgRating = avgRating(reviews)
+
 
 
   useEffect(()=> {
@@ -27,27 +43,42 @@ export default function OneSpot(){
   }, [dispatch])
 
   return (
-    <div>
+    <div className="oneSpot-details-container">
       <div className="details-container">
-        <h1>{filteredSpot?.description}</h1>
-        <button onClick={() => history.push(`/spots/${filteredSpot.id}/edit`)}>Edit</button>
-        <p>{filteredSpot?.city}, {filteredSpot?.state}, {filteredSpot?.country}</p>
+        <EditSpotForm filteredSpot={filteredSpot}/>
+        <div>
+
+          {/* <button onClick={() => history.push(`/spots/${filteredSpot.id}/edit`)}>Edit</button> */}
+
+        </div>
         <div className="spotImages-container">
-          <p>Images placeholder div</p>
+          <div className="spot-description-container">
+            <h2 className="spot-description">{filteredSpot?.name}</h2>
+          </div>
+          <div>
+            <p>{spotAvgRating} stars - {reviewsLength} Review(s) {filteredSpot?.city}, {filteredSpot?.state}, {filteredSpot?.country}</p>
+          </div>
+
           {spotImages?.map(spotImage => (
             <div className="spotImage-container">
-            <img src={spotImage?.url}></img>
+            <img className="details-image"src={spotImage?.url}></img>
+
+              <div>
+                <h4>{filteredSpot?.description}</h4>
+                <h4>${filteredSpot?.price.toString()}</h4>
+              </div>
             </div>
           ))}
         </div>
       </div>
 
       <div className="reviews-container">
-        <h3>Reviews go here</h3>
+        <h3>Reviews:</h3>
+
         {reviews?.map(review => (
-          <div>
+          <div className="single-review">
             <p>{review.body}</p>
-            <p>Rating: {review.rating}</p>
+            <p>Rating: {review.ratings}</p>
             <p>User: {review.user_id}</p>
           </div>
         ))}
