b0776612f0f2b36bdcfec46fb84b7ae8cc62ea64
christopher <cpannella3@gmail.com>
2022-12-22 17:17:33 -0500

    sauce

---
+++ app/api/booking_routes.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, jsonify, session, request
+from app.models import User, db, Booking
+from app.forms.booking_form import BookingForm
+
+
+booking_routes = Blueprint("bookings", __name__)
+
+
+@booking_routes.route('/')
+
+def get_all_bookings():
+  bookings = Booking.query.all()
+
+  response = {"bookings": [booking.to_dict() for booking in bookings]}
