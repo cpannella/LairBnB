c9cd050f564046e46f90f3323a82d0d94b45a19e
christopher <cpannella3@gmail.com>
2022-12-23 13:45:25 -0500

    bookings

--- app/forms/booking_form.py
+++ app/forms/booking_form.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms.fields import StringField, IntegerField, BooleanField, SubmitField, DateField, SelectField
+from wtforms.fields import StringField, IntegerField, BooleanField, SubmitField, DateField, SelectField, DateTimeField
 from wtforms.validators import DataRequired
 
 class BookingForm(FlaskForm):
-  start_date = DateField("Start Date", validators=[DataRequired()])
-  end_date = DateField("End Date", validators=[DataRequired()])
+  start_date = DateTimeField("Start Date", format='%Y-%m-%d %H:%M:%S', validators=[DataRequired()])
+  end_date = DateTimeField("End Date", format='%Y-%m-%d %H:%M:%S',validators=[DataRequired()])
   spot_id = IntegerField("Spot Id", validators=[DataRequired()])
