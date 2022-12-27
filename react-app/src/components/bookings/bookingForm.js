570216709ce1c3086bfe3a453aff697391163b46
christopher <cpannella3@gmail.com>
2022-12-27 14:18:14 -0500

    booking date parser done

--- react-app/src/components/bookings/bookingForm.js
+++ react-app/src/components/bookings/bookingForm.js
@@ -1,39 +1,88 @@
+import { isFirstDayOfMonth } from "date-fns";
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
-
+import { useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
 import "react-datepicker/dist/react-datepicker.css";
+import { createBookingThunk } from "../../store/bookings";
 
 // CSS Modules, react-datepicker-cssmodules.css
 // import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 
   function CreateBookingForm(){
+  const {id} = useParams()
+  console.log("THIS IS THE ID",id)
+  const dispatch = useDispatch()
+  const [startDate, setStartDate] = useState(new Date("2022-12-25"));
+  const [endDate, setEndDate] = useState(new Date("2022-12-25"));
 
-  const [startDate, setStartDate] = useState(new Date("2014/02/08"));
-  const [endDate, setEndDate] = useState(new Date("2014/02/10"));
+    //date needs to be "Year-Month-Day Hour:Minunte:Second"
+    //starts as {startDate: Sat Feb 08 2014 00:00:00 GMT-0500 (Eastern Standard Time), endDate: Mon Feb 10 2014 00:00:00 GMT-0500 (Eastern Standard Time)}
+  function dateParser(target){
+    let str = target.toString()
+    let arr = str.split(' ')
+    let result = []
+    for(let i = 1; i < 5; i++){
+      console.log(arr[i])
+      if(arr[i] == "Jan") arr[i] = '01'
+      else if (arr[i] == 'Feb') arr[i] = '02'
+      else if (arr[i] == 'Mar') arr[i] = '03'
+      else if (arr[i] == 'Apr') arr[i] = '04'
+      else if (arr[i] == 'May') arr[i] = '05'
+      else if (arr[i] == 'Jun') arr[i] = '06'
+      else if (arr[i] == 'Jul') arr[i] = '07'
+      else if (arr[i] == 'Aug') arr[i] = '08'
+      else if (arr[i] == 'Sep') arr[i] = '09'
+      else if (arr[i] == 'Oct') arr[i] = '10'
+      else if (arr[i] == 'Nov') arr[i] = '11'
+      else if (arr[i] == 'Dec') arr[i] = '12'
+    }
 
+    for(let j = 1; j < 4;j++){
+      result.push(arr[j])
+    }
+   let newDate = result.join('-')
+   newDate += ` ${arr[4]}`
+   return newDate
+  }
 
-   
+  const onSubmit = async (e) => {
+    e.preventDefault()
+    dateParser(startDate)
+    const payload = {
+      startDate: dateParser(startDate),
+      endDate: dateParser(endDate)
+    }
+    console.log(payload)
+    let bookingCreated = await dispatch(createBookingThunk(payload, id))
+    setStartDate(new Date("2022-12-25"))
+    setEndDate(new Date("2022-12-25"))
+  }
+
 
 
 
   return (
-  <div>
-    <DatePicker
-      selected={startDate}
-      onChange={(date) => setStartDate(date)}
-      selectsStart
-      startDate={startDate}
-      endDate={endDate}
-    />
-    <DatePicker
-      selected={endDate}
-      onChange={(date) => setEndDate(date)}
-      selectsEnd
-      startDate={startDate}
-      endDate={endDate}
-      minDate={startDate}
-    />
+  <form onSubmit={onSubmit}>
+    <div>
+      <DatePicker
+        selected={startDate}
+        onChange={(date) => setStartDate(date)}
+        selectsStart
+        startDate={startDate}
+        endDate={endDate}
+      />
+      <DatePicker
+        selected={endDate}
+        onChange={(date) => setEndDate(date)}
+        selectsEnd
+        startDate={startDate}
+        endDate={endDate}
+        minDate={startDate}
+      />
+      <button type="submit">Submit</button>
   </div>
+ </form>
   )
 };
 
--- react-app/src/store/bookings.js
+++ react-app/src/store/bookings.js
@@ -12,7 +12,7 @@ const oneBooking = payload => {
   }
 }
 
-const allReviews = payload => {
+const allBookings = payload => {
   return {
       type: ALL_BOOKINGS,
       payload
@@ -39,3 +39,111 @@ const deleteBooking = (booking) => {
       booking
   }
 }
+
+
+
+export const fetchBookings = () => async dispatch => {
+  const response = await fetch('/api/spots/bookings')
+
+  if(response.ok){
+    const bookings = await response.json()
+    dispatch(allBookings(bookings))
+    return bookings
+  }
+}
+
+
+export const fetchOneBooking = (id) => async dispatch => {
+  const response = await fetch(`/api/spots/bookings/${id}`)
+  if(response.ok){
+    const singleBooking = await response.json()
+    dispatch(oneBooking(singleBooking))
+    return singleBooking
+  }
+}
+
+
+export const createBookingThunk = (payload, id) => async dispatch => {
+  const response = await fetch(`/api/spots/${id}/new_booking`,{
+    method: 'POST',
+    headers: {'Content-Type': 'application/json' },
+    body: JSON.stringify(payload)
+  })
+  if(response.ok){
+    const newBooking = await response.json()
+    dispatch(createBooking(newBooking))
+    return newBooking
+  }
+}
+
+
+
+export const editBookingThunk = (booking, id) => async dispatch => {
+  const response = await fetch(`/api/spots/bookings/${id}/edit`, {
+    method: "PUT",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(booking)
+  })
+
+  if(response.ok){
+    const booking = await response.json()
+    dispatch(editBooking(booking))
+    return booking
+  }
+}
+
+export const deleteReviewThunk = (id) => async dispatch => {
+  const response = await fetch(`/api/spots/bookings/${id}`, {
+    method: "DELETE",
+    headers: {"Content-Type": "application/json"}
+  })
+
+  if(response.ok){
+    const booking = `${id}`
+    dispatch(deleteBooking(booking))
+  }
+}
+
+
+const initialState = {}
+
+  function bookingReducer(state = initialState, action){
+    let newState;
+
+    switch(action.type){
+      case ALL_BOOKINGS:{
+        newState = {...state}
+        action.payload.bookings.forEach(booking => {
+          newState[booking.id] = booking
+        })
+        return newState
+      }
+      case ONE_BOOKING:{
+        newState = {...state}
+        newState[action.payload.id] = action.payload
+        return newState
+      }
+      case CREATE_BOOKING: {
+        newState = {...state}
+        newState[action.payload.id] = action.payload
+        return newState
+      }
+
+      case EDIT_BOOKING:{
+        newState = {...state}
+        newState[action.booking.id] = action.booking
+        return newState
+      }
+      case DELETE_BOOKING: {
+        newState = {...state}
+        delete newState[action.booking]
+        return newState
+      }
+
+      default: {
+        return state
+      }
+    }
+  }
+
+  export default bookingReducer
