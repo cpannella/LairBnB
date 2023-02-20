a2ce4ce224b3d3206806dec1c4f3652045792f56
christopher <cpannella3@gmail.com>
2023-02-20 10:19:59 -0700

    update

--- react-app/src/components/spots/oneSpot.js
+++ react-app/src/components/spots/oneSpot.js
@@ -14,6 +14,8 @@ import CreateBookingForm from "../bookings/bookingForm";
 
 
 export default function OneSpot(){
+
+  
   const dispatch = useDispatch()
   const history = useHistory()
   const {id} = useParams()
