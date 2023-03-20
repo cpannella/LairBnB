cfc878d2f77e0bec831a8003ffb4f0a869761659
christopher <cpannella3@gmail.com>
2023-03-20 10:59:52 -0600

    formatting fixes

--- react-app/src/components/spots/oneSpot.js
+++ react-app/src/components/spots/oneSpot.js
@@ -1,29 +1,18 @@
-
-import React from "react";
-import { useEffect, useState } from "react";
-import profile from './profile.jpeg'
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { NavLink, useHistory, useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import { deleteReviewThunk, fetchReviews } from "../../store/reviews";
-import { fetchOneSpot, fetchSpots } from "../../store/spots";
+import { fetchOneSpot } from "../../store/spots";
+import profile from './profile.jpeg'
 import EditReviewForm from "../reviews/editReviewForm";
 import ReviewForm from "../reviews/reviewForm";
 import EditSpotFormModal from "../spotForms/editSpotFormModal";
-import "./spotsIndex.css"
 import defaultPic from './default.jpg'
 import CreateBookingForm from "../bookings/bookingForm";
-
+import "./spotsIndex.css"
 
 export default function OneSpot(){
-
-
-
-
-
   const dispatch = useDispatch()
-  const history = useHistory()
-  const {id} = useParams()
-  const thisUser = useSelector(state => state.session.user);
   const spotsState = useSelector(state => state.spots)
   const spots = Object.values(spotsState)
   const reviewsState = useSelector(state => state.reviews)
@@ -33,12 +22,7 @@ export default function OneSpot(){
   const filteredSpot = spots?.filter(spot => spot.id == +id)[0]
   const sessionUser = useSelector(state => state.session)
   const filteredReviews = reviews.filter(review => review.spot_id == id)
-  const reviewsLength = reviews?.length
-  // const spotImages = filteredSpot?.spotImages
-
-  console.log(filteredReviews)
   const user = filteredSpot?.user[0].username
-
 
   const avgRating = (arr) => {
     if(arr?.length == 0){
@@ -53,9 +37,6 @@ export default function OneSpot(){
   }
 
   const monthParser = (s) => {
-
-    if(s) console.log("THIS IS THE FUNCTIOn",s)
-
     if(s == 'Jan') s = 'January'
     if(s == 'Feb') s = 'February'
     if(s == 'Mar') s = 'March'
@@ -72,11 +53,7 @@ export default function OneSpot(){
     return s
   }
 
-
   const spotAvgRating = avgRating(filteredReviews)
-
-
-
 
   useEffect(()=> {
     dispatch(fetchOneSpot(id))
@@ -89,50 +66,38 @@ export default function OneSpot(){
         <div className="spotImages-container">
           <div className="spot-description-container">
             <div className="stuff">
-            <h2 className="spot-description">{filteredSpot?.name}</h2>
-            {sessionUser.user && sessionUser?.user.id === filteredSpot?.user_id &&
-            <div className="button-div">
-            <EditSpotFormModal id="edit-modal-button"filteredSpot={filteredSpot}/>
-            </div>
-            }
+              <h2 className="spot-description">{filteredSpot?.name}</h2>
+                {sessionUser.user && sessionUser?.user.id === filteredSpot?.user_id &&
+                <div className="button-div">
+                <EditSpotFormModal id="edit-modal-button"filteredSpot={filteredSpot}/>
+                </div>
+                }
             </div>
           </div>
           <div className="rating-wrapper">
             <p>{spotAvgRating}<i id="upper-star" class="fa-sharp fa-solid fa-star"></i></p> <p className="details-span">{filteredReviews.length}  Review(s)</p> <p className="details-span-two">{filteredSpot?.city}, {filteredSpot?.state}, {filteredSpot?.country}</p>
           </div>
-          {/* <div className="stuff">
-            <p>{spotAvgRating}<i id="star" class="fa-sharp fa-solid fa-star"></i></p> <p className="details-span">{filteredReviews.length}  Review(s)</p> <p className="details-span-two">{filteredSpot?.city}, {filteredSpot?.state}, {filteredSpot?.country}</p>
-          </div> */}
-
-
-
           <div className="spotImage-container">
                <div className="spotImage-container">
-                  <img className="details-image"src={filteredSpot?.url}
-                   onError={e => {e.target.src=`${defaultPic}`}}></img>
-                    <div className="someDiv">
-                      <div className="description-container">
-                        <h2 id="entire">Entire lair hosted by {user.charAt(0).toUpperCase() + user.slice(1)}</h2>
-                        <div className="booking-form-container">
-                            <CreateBookingForm filteredSpot={filteredSpot}/>
-                        </div>
-                      </div>
-                      <div className="lair-cover">
-                        <div className="inner-lair-cover">
-                          <p id="cover-red" className="title">lair</p><p className="title">cover</p>
-                        </div>
-                        <p>Every booking includes free protection from Host cancellations, listing inaccuracies, and other issues like trouble checking in.</p>
-                      </div>
-                      <div className="description-cover">
-                        <p>{filteredSpot?.description}</p>
-                      </div>
-
-                      {/* <div className="booking-form-container">
-                          <div className="something">
-                            <CreateBookingForm filteredSpot={filteredSpot}/>
-                          </div>
-                      </div> */}
+                <img className="details-image"src={filteredSpot?.url}
+                onError={e => {e.target.src=`${defaultPic}`}}></img>
+                <div className="someDiv">
+                  <div className="description-container">
+                    <h2 id="entire">Entire lair hosted by {user.charAt(0).toUpperCase() + user.slice(1)}</h2>
+                    <div className="booking-form-container">
+                        <CreateBookingForm filteredSpot={filteredSpot}/>
                     </div>
+                  </div>
+                  <div className="lair-cover">
+                    <div className="inner-lair-cover">
+                      <p id="cover-red" className="title">lair</p><p className="title">cover</p>
+                    </div>
+                    <p>Every booking includes free protection from Host cancellations, listing inaccuracies, and other issues like trouble checking in.</p>
+                  </div>
+                  <div className="description-cover">
+                    <p>{filteredSpot?.description}</p>
+                  </div>
+                </div>
                </div>
            </div>
         </div>
@@ -143,10 +108,8 @@ export default function OneSpot(){
             {sessionUser.user && sessionUser.user.id !== filteredSpot?.user_id &&
             <div>
             <ReviewForm />
-            </div>
-            }
-          </div>
-          }
+            </div>}
+          </div>}
           {filteredReviews?.length > 0 &&
           <div>
             <div className="review-title">
@@ -156,41 +119,32 @@ export default function OneSpot(){
 
             {filteredReviews?.map(review => (
               <div className="single-review">
-                    <div className="review-details">
-
-                      <div className="profile-container">
-
-                        <img className="profile"src={profile}></img>
-
-                        <div>
-                          <h4 className="review-username">{review?.user[0]?.username.charAt(0).toUpperCase() + review?.user[0]?.username.slice(1)}</h4>
-                          <p className="review-username">{monthParser(review.created_at.slice(8,11))} {review.created_at.substring(12, 16)}</p>
-                        </div>
-
-                      </div>
-
-                      <p className="review-body">{review.body}</p>
-                      {/* <p>Rating: {review.rating}</p> */}
-                    </div>
-                    <div className="fugazi">
-                    {sessionUser.user && sessionUser?.user.id === review?.user_id &&
+                <div className="review-details">
+                  <div className="profile-container">
+                    <img className="profile"src={profile}></img>
                     <div>
-                      <button onClick={()=> [setShowEditReviewForm(true), setEditReviewId(review.id)]}>Edit</button>
+                      <h4 className="review-username">{review?.user[0]?.username.charAt(0).toUpperCase() + review?.user[0]?.username.slice(1)}</h4>
+                      <p className="review-username">{monthParser(review.created_at.slice(8,11))} {review.created_at.substring(12, 16)}</p>
                     </div>
-                    }
-                    {sessionUser.user && sessionUser?.user.id === review?.user_id &&
-                    <div>
-                      <button className="delete-button"onClick={()=> dispatch(deleteReviewThunk(review.id))}>Delete</button>
-                    </div>}
-                    </div>
+                  </div>
+                  <p className="review-body">{review.body}</p>
+                </div>
+                <div className="fugazi">
+                {sessionUser.user && sessionUser?.user.id === review?.user_id &&
+                <div>
+                  <button onClick={()=> [setShowEditReviewForm(true), setEditReviewId(review.id)]}>Edit</button>
+                </div>
+                }
+                {sessionUser.user && sessionUser?.user.id === review?.user_id &&
+                <div>
+                  <button className="delete-button"onClick={()=> dispatch(deleteReviewThunk(review.id))}>Delete</button>
+                </div>}
+                </div>
 
-                    {showEditReviewForm  && review.id == editReviewId &&
-                    <div className="edit-buttons">
-                    <EditReviewForm review={review} setShowEditReviewForm={setShowEditReviewForm}></EditReviewForm>
-                    {/* <button onClick={()=> setShowEditReviewForm(false)}>Cancel</button> */}
-                    </div>}
-
-
+                {showEditReviewForm  && review.id == editReviewId &&
+                <div className="edit-buttons">
+                <EditReviewForm review={review} setShowEditReviewForm={setShowEditReviewForm}></EditReviewForm>
+                </div>}
               </div>
             ))}
              {sessionUser.user && sessionUser.user.id !== filteredSpot?.user_id &&
@@ -203,8 +157,6 @@ export default function OneSpot(){
         }
         </div>
       </div>
-
-
     </div>
   )
 }
